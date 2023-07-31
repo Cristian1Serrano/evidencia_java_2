f2c726526850baddeecfb7dc0c78ad720836c892
lancero <casu042010.casu@gmail.com>
2023-07-31 10:27:30 -0500

    evidencia java 2

--- src/java/controladores/RecibeDatos.java
+++ src/java/controladores/RecibeDatos.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controladores;
 
 import java.io.IOException;
@@ -12,74 +8,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
- * @author Creat
+ * Servlet implementation class RecibeDatos
  */
 public class RecibeDatos extends HttpServlet {
-
+    private static final long serialVersionUID = 1L;
+       
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
+        try (PrintWriter out = response.getWriter()) {
+            // Recuperar los parámetros del formulario utilizando el método "GET"
+            String nombre = request.getParameter("nombre");
+            String contrasena = request.getParameter("contrasena");
+            
+            // Mostrar los datos recibidos en la página
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet RecibeDatos</title>");            
+            out.println("<title>Formulario Recibido</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet RecibeDatos at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Datos Recibidos:</h1>");
+            out.println("<p>Nombre de usuario: " + nombre + "</p>");
+            out.println("<p>Contraseña: " + contrasena + "</p>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            // Recuperar los parámetros del formulario utilizando el método "POST"
+            String nombre = request.getParameter("nombre");
+            String contrasena = request.getParameter("contrasena");
+            
+            // Mostrar los datos recibidos en la página
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Formulario Recibido</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Datos Recibidos:</h1>");
+            out.println("<p>Nombre de usuario: " + nombre + "</p>");
+            out.println("<p>Contraseña: " + contrasena + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
